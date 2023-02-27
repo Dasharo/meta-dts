da13dcc29e7d3437691b0eb34a5f2e302210da40
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-02-27 18:21:06 +0100

    dts: move color functions to dts-functions.sh
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- meta-dts-distro/recipes-dts/dts/dts/dts-functions.sh
+++ meta-dts-distro/recipes-dts/dts/dts/dts-functions.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# Text Reset
+COLOR_OFF='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+### Color functions
+print_warning() {
+  echo -e "$YELLOW""$1""$COLOR_OFF"
+}
+
+print_error() {
+  echo -e "$RED""$1""$COLOR_OFF"
+}
+
+print_green() {
+  echo -e "$GREEN""$1""$COLOR_OFF"
+}
+
 check_if_dasharo() {
   if [[ $BIOS_VENDOR == *$DASHARO_VENDOR* && $BIOS_VERSION == *$DASHARO_NAME* ]]; then
     return 0
