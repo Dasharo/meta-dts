f19ec50febf427b5bec9ce5b72a84eba941e946c
Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>
2023-05-09 15:32:52 +0200

    distro: dts: store flashrom logs in /var/local/flashrom.log file
    Signed-off-by: Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>

--- meta-dts-distro/recipes-dts/dts/dts/dts-environment.sh
+++ meta-dts-distro/recipes-dts/dts/dts/dts-environment.sh
@@ -6,6 +6,11 @@ BOARD_MODEL="$(dmidecode -s baseboard-product-name)"
 
 BIOS_VENDOR="$(dmidecode -s bios-vendor)"
 BIOS_VERSION="$(dmidecode -s bios-version)"
+DASHARO_VERSION="$(echo $BIOS_VERSION | cut -d ' ' -f 3 | tr -d 'v')"
+
+# path to log files
+ERR_LOG_FILE="/var/local/dts-err.log"
+FLASHROM_LOG_FILE="/var/local/flashrom.log"
 
 # variables defining Dasharo specific entires in DMI tables, used to check if
 # Dasharo FW is already installed
