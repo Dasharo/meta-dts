a48629034b49ea261b67aba8c61d4168136c829c
Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>
2024-01-04 09:53:24 +0100

    scripts: get_last_commit: add script to get latest commit from given branch
    Signed-off-by: Tomasz Żyjewski <tomasz.zyjewski@3mdeb.com>

---
+++ scripts/get_last_commit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts "r:b:" arg; do
+    case $arg in
+        r)
+            repo="${OPTARG}"
+            ;;
+        b)
+            branch="${OPTARG}"
+            ;;
+    esac
+done
+
+die() {
+    echo "$@"
+    exit 1
+}
+
+[ -n "${repo}" ] || die repo not set
+[ -n "${branch}" ] || die branch not set
+
+output=$(git ls-remote --heads "https://github.com/${repo}" "refs/heads/${branch}")
+[ "$?" -eq 0 ] || die failed to get commit info
+
+output=$(echo $output | grep -E "^[0-9a-z]+\s+refs/heads/${branch}$")
+[ "$?" -eq 0 ] || die failed to parse git output
+
+echo $output | awk '{ print $1 }'
