c1cf8dca75d8cb58faa0b60c7ba138354ad29511
Pawel Langowski <pawel.langowski@3mdeb.com>
2024-11-19 11:28:10 +0100

    Move generate-changelog.sh to scripts
    Signed-off-by: Pawel Langowski <pawel.langowski@3mdeb.com>

--- generate-changelog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 1 ]; then
-  docker run -t -v "$(pwd)":/app/ "orhunp/git-cliff:${TAG:-latest}" \
-    --prepend CHANGELOG.md --latest --github-token "$1"
-else
-  docker run -t -v "$(pwd)":/app/ "orhunp/git-cliff:${TAG:-latest}" \
-    --prepend CHANGELOG.md --latest
-fi
