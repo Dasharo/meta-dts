3f0836bd610418a71fd668871d703c6b080516e8
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2025-01-30 18:51:35 +0100

    generate-changelog.sh: remove container after use
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -26,9 +26,9 @@ REPO_DIR=$(git rev-parse --show-toplevel)
 RELEASE=v$(grep '^DISTRO_VERSION ' "$REPO_DIR/meta-dts-distro/conf/distro/dts-distro.conf" | cut -d'=' -f2 | tr -d ' "')
 
 if [ $# -eq 1 ]; then
-  docker run -t -v "$REPO_DIR":/app/ "orhunp/git-cliff:${TAG:-latest}" \
+  docker run -t --rm -v "$REPO_DIR":/app/ "orhunp/git-cliff:${TAG:-latest}" \
     --prepend CHANGELOG.md --unreleased --tag "$RELEASE" --github-token "$1"
 else
-  docker run -t -v "$REPO_DIR":/app/ "orhunp/git-cliff:${TAG:-latest}" \
+  docker run -t --rm -v "$REPO_DIR":/app/ "orhunp/git-cliff:${TAG:-latest}" \
     --prepend CHANGELOG.md --unreleased --tag "$RELEASE"
 fi
