b7020d7630312f482671c33c6d41c5afb3462aa7
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2025-01-30 18:47:01 +0100

    generate-changelog.sh: Add help
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+print_usage() {
+  cat <<EOF
+Usage: $(basename "$0") [GITHUB_TOKEN]
+
+  Update CHANGELOG.md so it contains all changes between previous release up to
+  current commit.
+  If you encounter API request limit errors when generating changelog you can
+  pass optional GITHUB_TOKEN argument containing your GitHub token which will
+  increase those limits.
+EOF
+}
+
+if [ $# -gt 1 ]; then
+  print_usage
+  exit 1
+fi
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  print_usage
+  exit 0
+fi
+
 REPO_DIR=$(git rev-parse --show-toplevel)
 RELEASE=v$(grep '^DISTRO_VERSION ' "$REPO_DIR/meta-dts-distro/conf/distro/dts-distro.conf" | cut -d'=' -f2 | tr -d ' "')
 
